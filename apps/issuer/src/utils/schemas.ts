448b0fcdd6c27ce1473ed0bd529e5a41d2bf3617
Tadej <tadej.podrekar@gmail.com>
2023-09-28 22:44:40 +0200

    feat: create validation schema

---
+++ apps/issuer/src/utils/schemas.ts
@@ -0,0 +1,80 @@
+export const CredentialSchema = {
+  type: 'object',
+  properties: {
+    proofFormat: {
+      type: 'string',
+      enum: ['jwt']
+    },
+    credential: {
+      type: 'object',
+      properties: {
+        "@context": {
+          type: 'array',
+          items: {
+            type: 'string',
+            enum: [
+              'https://www.w3.org/2018/credentials/v1',
+              'https://beta.api.schemas.serto.id/v1/public/program-completion-certificate/1.0/ld-context.json'
+            ]
+          },
+          minItems: 2,
+          uniqueItems: true
+        },
+        type: {
+          type: 'array',
+          items: {
+            type: 'string',
+            enum: [
+              'VerifiableCredential',
+              'ProgramCompletionCertificate'
+            ]
+          },
+          minItems: 2,
+          uniqueItems: true
+        },
+        credentialSubject: {
+          type: 'object',
+          properties: {
+            accomplishmentType: {
+              'type': 'string'
+            },
+            learnerName: {
+              'type': 'string'
+            },
+            achievement: {
+              'type': 'string'
+            },
+            courseProvider: {
+              'type': 'string'
+            },
+            id: {
+              'type': 'string'
+            }
+          },
+          required: [
+            'accomplishmentType',
+            'learnerName',
+            'achievement',
+            'courseProvider',
+            'id'
+          ]
+        },
+        credentialSchema: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string'
+            },
+            type: {
+              type: 'string',
+              enum: ['JsonSchemaValidator2018']
+            }
+          },
+          required: ['id', 'type']
+        }
+      },
+      required: ['@context', 'type', 'credentialSubject', 'credentialSchema']
+    }
+  },
+  required: ['proofFormat', 'credential']
+}
