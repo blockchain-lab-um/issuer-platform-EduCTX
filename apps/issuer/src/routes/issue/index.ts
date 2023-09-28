fc14ae80c7f6b3691d8b865a2f4a36503f6c95b6
Tadej <tadej.podrekar@gmail.com>
2023-09-28 22:44:24 +0200

    feat: add issue route

--- apps/issuer/src/routes/issue/index.ts
+++ apps/issuer/src/routes/issue/index.ts
@@ -1,7 +1,23 @@
 import { FastifyPluginAsync } from 'fastify';
+import { CredentialSchema } from '../../utils/schemas.js';
 
-const issue: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async (request, reply) => 'Issue');
+const issue: FastifyPluginAsync = async (fastify): Promise<void> => {
+  // Add attachValidation to the schema if you want to handle validation errors yourself
+  fastify.post('/', { schema: { body: CredentialSchema } }, async (request, reply) => {
+    const data = request.body as any; // TODO: fix type
+    const agent = fastify.veramoAgent();
+    const issuerIdentifier = await agent.didManagerGetByAlias({ alias: 'issuer-primary', provider: 'did:key' });
+
+    const vc = await agent.createVerifiableCredential({
+      credential: {
+        issuer: issuerIdentifier.did,
+        ...data.credential,
+      },
+      proofFormat: data.proofFormat,
+    });
+
+    await reply.code(200).send({ data, vc });
+  });
 };
 
 export default issue;
