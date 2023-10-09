f51e6e200fb1f1fd2e93905a61000962737cc4d2
Martin Domajnko <domajnko.martin@gmail.com>
2023-10-09 11:50:01 +0200

    feat: replace github with AzureAD provider

--- apps/dashboard/src/lib/auth.ts
+++ apps/dashboard/src/lib/auth.ts
@@ -1,14 +1,14 @@
 import { NextAuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import AzureADProvider from 'next-auth/providers/azure-ad';
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID!,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
+      tenantId: process.env.AZURE_AD_TENANT_ID!,
     }),
-    // ...add more providers here
   ],
 };
