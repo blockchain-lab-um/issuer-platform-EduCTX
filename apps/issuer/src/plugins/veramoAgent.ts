39f4e778881ff04a7c49849aaf76850a20bca7af
Tadej <tadej.podrekar@gmail.com>
2023-10-19 15:21:21 +0200

    feat: switch to did ebsi

--- apps/issuer/src/plugins/veramoAgent.ts
+++ apps/issuer/src/plugins/veramoAgent.ts
@@ -2,6 +2,7 @@ import {
   KeyDIDProvider,
   getDidKeyResolver as keyDidResolver,
 } from '@blockchain-lab-um/did-provider-key';
+import { EbsiDIDProvider, ebsiDidResolver } from '@blockchain-lab-um/did-provider-ebsi';
 import {
   AbstractDataStore,
   DataManager,
@@ -38,28 +39,29 @@ import fp from 'fastify-plugin';
 
 export type Agent = TAgent<
   IDIDManager &
-    IKeyManager &
-    IDataStore &
-    IResolver &
-    ICredentialIssuer &
-    ICredentialVerifier
+  IKeyManager &
+  IDataStore &
+  IResolver &
+  ICredentialIssuer &
+  ICredentialVerifier
 >;
 
 async function createVeramoAgent() {
   const didProviders: Record<string, AbstractIdentifierProvider> = {};
   const vcStorePlugins: Record<string, AbstractDataStore> = {};
 
-  didProviders['did:key'] = new KeyDIDProvider({ defaultKms: 'web3' });
+  didProviders['did:key'] = new KeyDIDProvider({ defaultKms: 'local' });
+  didProviders['did:ebsi'] = new EbsiDIDProvider({ defaultKms: 'local' });
 
   vcStorePlugins.memory = new MemoryDataStore();
 
   return createAgent<
     IDIDManager &
-      IKeyManager &
-      IDataStore &
-      IResolver &
-      ICredentialIssuer &
-      ICredentialVerifier
+    IKeyManager &
+    IDataStore &
+    IResolver &
+    ICredentialIssuer &
+    ICredentialVerifier
   >({
     plugins: [
       new CredentialPlugin(),
@@ -73,6 +75,7 @@ async function createVeramoAgent() {
       new DIDResolverPlugin({
         resolver: new Resolver({
           ...keyDidResolver(),
+          ...ebsiDidResolver(),
         }),
       }),
       new DIDManager({
@@ -86,27 +89,48 @@ async function createVeramoAgent() {
 
 export default fp<FastifyPluginOptions>(async (fastify, _opts) => {
   const agent = await createVeramoAgent();
-  await agent.didManagerImport({
-    did: `did:key:${process.env.ISSUER_ADDRESS}`,
-    provider: 'did:key',
-    controllerKeyId: 'primary',
-    alias: 'issuer-primary',
-    keys: [
-      {
-        kid: 'primary',
-        type: 'Secp256k1',
-        kms: 'local',
-        privateKeyHex: process.env.ISSUER_PRIVATE_KEY,
-      } as MinimalImportableKey,
-    ],
-  });
-  const issuerIdentifier = await agent.didManagerGetByAlias({
-    alias: 'issuer-primary',
-    provider: 'did:key',
-  });
+  let identifier: IIdentifier;
+
+  if (process.env.EBSI_BEARER_TOKEN && process.env.EBSI_PRIVATE_KEY && process.env.EBSI_ID) {
+    identifier = await agent.didManagerCreate({
+      provider: 'did:ebsi',
+      alias: 'ebsi',
+      kms: 'local',
+      options: {
+        bearer: process.env.EBSI_BEARER_TOKEN,
+        privateKeyHex: process.env.EBSI_PRIVATE_KEY,
+        keyType: 'P-256',
+        id: process.env.EBSI_ID
+      },
+    });
+
+    console.log(`Your issuer EBSI did is: ${identifier.did}`);
+  } else {
+    await agent.didManagerImport({
+      did: `did:key:${process.env.ISSUER_ADDRESS}`,
+      provider: 'did:key',
+      controllerKeyId: 'primary',
+      alias: 'issuer-primary',
+      keys: [
+        {
+          kid: 'primary',
+          type: 'Secp256k1',
+          kms: 'local',
+          privateKeyHex: process.env.ISSUER_PRIVATE_KEY,
+        } as MinimalImportableKey,
+      ],
+    });
+
+    identifier = await agent.didManagerGetByAlias({
+      alias: 'issuer-primary',
+      provider: 'did:key',
+    });
+  }
+
+  if (!identifier) throw new Error('Cannot generate valid identifier')
 
   fastify.decorate('veramoAgent', () => agent);
-  fastify.decorate('issuerIdentifier', () => issuerIdentifier);
+  fastify.decorate('issuerIdentifier', () => identifier);
 });
 
 declare module 'fastify' {
