b106b04bb9f78363564a6b2aec02303c15d7190c
Martin Domajnko <domajnko.martin@gmail.com>
2023-10-27 15:40:42 +0200

    feat: missing docker-compose added

--- README.md
+++ README.md
@@ -4,3 +4,19 @@
 
 - Node.js v18.17.1
 - pnpm 8.7.6
+
+### Docker
+
+We are using 3 images:
+
+- Base image for building everything in the monorepo: `blockchain-lab-um/eductx-platform-base`
+- Dashboard image: `blockchain-lab-um/eductx-platform-dashboard`
+- Issuer image: `blockchain-lab-um/eductx-platform-issuer`
+
+We also have `docker-compose.yml` file prepared, but you need to build the base image first.
+
+This can be done with the following command: `pnpm docker:build`
+
+If you already have `.env` files for the issuer and dashboard you can run them using docker compose with the following command:
+
+- `docker compose --env-file apps/dashboard/.env --env-file apps/issuer/.env up --build`
---
+++ apps/issuer/src/server.ts
@@ -0,0 +1,39 @@
+import * as dotenv from "dotenv";
+
+// Require the framework
+import Fastify from "fastify";
+
+// Require library to exit fastify process, gracefully (if possible)
+import closeWithGrace from "close-with-grace";
+
+// Read the .env file.
+dotenv.config();
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+});
+
+// Register your application as a normal plugin.
+await app.register(import("./app.js"));
+
+// delay is the number of milliseconds for the graceful close to finish
+const closeListeners = closeWithGrace({ delay: 500 }, (async ({ err })  => {
+  if (err) {
+    app.log.error(err)
+  }
+  await app.close()
+}) as closeWithGrace.CloseWithGraceAsyncCallback);
+
+app.addHook('onClose', (_, done) => {
+  closeListeners.uninstall();
+  done();
+})
+
+// Start listening.
+app.listen({ port: parseInt(process.env.PORT ?? "3001", 10) }, (err: any) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+});
