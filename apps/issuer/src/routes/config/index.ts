eea0fd04af8731a90524dda7bc3cf0f27d6a4da7
Tadej <tadej.podrekar@gmail.com>
2023-11-17 12:43:15 +0100

    chore: remove unused endpoints

--- apps/issuer/src/routes/config/index.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyPluginAsync } from 'fastify';
-
-const config: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async (request, reply) => 'Config');
-};
-
-export default config;
