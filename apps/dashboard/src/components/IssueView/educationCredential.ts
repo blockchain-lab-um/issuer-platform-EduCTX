857d9f44ee18eca6c207b0027197c0adbccc9088
Urban Vidovič <urbanfoundit@gmail.com>
2023-11-22 14:12:00 +0100

    chore: rename EduCredentialSchema
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- apps/dashboard/src/components/IssueView/educationCredential.ts
+++ apps/dashboard/src/components/IssueView/educationCredential.ts
@@ -130,7 +130,7 @@ interface Schema {
   fields: SchemaNode[] | SchemaObject[];
 }
 
-export const EduCredentialSchema = {
+export const EducationalCredentialSchema = {
   title: 'Education Credential',
   type: '#educationCredential',
   fields: [
