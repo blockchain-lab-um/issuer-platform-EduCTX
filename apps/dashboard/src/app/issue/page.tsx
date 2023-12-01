238d70b2192d228ff194aaef0df391a286988d9a
martines3000 <domajnko.martin@gmail.com>
2023-12-01 12:57:10 +0100

    feat: add middleware for auth on dashboard

--- apps/dashboard/src/app/issue/page.tsx
+++ apps/dashboard/src/app/issue/page.tsx
@@ -1,32 +1,9 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import { Button } from '@nextui-org/react';
-import { useSession } from 'next-auth/react';
-
 import { IssueView } from '@/components/IssueView';
 
-export default function Page() {
-  const router = useRouter();
-  const { data: session, status } = useSession();
-
+export default async function Page() {
   return (
     <div className="">
-      {session && <IssueView />}
-      {!session && (
-        <div>
-          Login to continue
-          <Button
-            variant="bordered"
-            size="sm"
-            onClick={() => {
-              router.push('/');
-            }}
-          >
-            Login
-          </Button>
-        </div>
-      )}
+      <IssueView />
     </div>
   );
 }
