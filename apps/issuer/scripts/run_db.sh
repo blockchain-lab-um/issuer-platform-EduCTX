cf8d6fd4b713724f9f2080943d3b8b54b88a019e
martines3000 <domajnko.martin@gmail.com>
2024-01-10 16:33:28 +0100

    fix: change some ports

--- apps/issuer/scripts/run_db.sh
+++ apps/issuer/scripts/run_db.sh
@@ -9,7 +9,7 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 # Check if a custom database name has been set, otherwise default to 'credentials'
 DB_NAME="${POSTGRES_DB:=credentials}"
 # Check if a custom port has been set, otherwise default to '5432'
-DB_PORT="${POSTGRES_PORT:=5432}"
+DB_PORT="${POSTGRES_PORT:=34569}"
 
 # Launch postgres using Docker
 # Allow to skip Docker if a dockerized Postgres database is already running
