c361ca92f79b4af3e2180189d308c4eb590ecfbb
martines3000 <domajnko.martin@gmail.com>
2024-08-12 11:56:19 +0200

    feat: finished design of basic credential

--- apps/dashboard/src/components/EmailTemplates/BasicEmail.tsx
+++ apps/dashboard/src/components/EmailTemplates/BasicEmail.tsx
@@ -11,13 +11,10 @@ import {
   Link,
   Section,
   Hr,
+  Head,
 } from '@react-email/components';
 import { tailwindConfig } from './tailwindConfig';
 import {
-  DevicePhoneMobileIcon,
-  WalletIcon,
-  QrCodeIcon,
-  KeyIcon,
   GlobeAltIcon,
   LockClosedIcon,
   ForwardIcon,
@@ -33,22 +30,17 @@ export const BasicEmail: React.FC<Readonly<BasicEmailProps>> = ({
 }) => (
   <Tailwind config={tailwindConfig}>
     <Html lang="en">
+      <Head />
       <Body>
-        <Container className="w-[390px]">
+        <Container>
           <Section className="my-[22px]">
             <Row>
-              <Heading
-                as="h1"
-                className="text-center font-medium text-2xl px-7"
-              >
+              <Heading as="h1" className="text-center font-bold text-3xl px-7">
                 You recieved a new credential!
               </Heading>
             </Row>
             <Row>
-              <Text
-                style={{ fontSize: '10px', lineHeight: '12px' }}
-                className="text-center px-7"
-              >
+              <Text className="text-center px-7 text-xs">
                 The credential has been issued to you via EduCTX platform.
                 Retreive it anytime with the QR code below.
               </Text>
@@ -59,13 +51,13 @@ export const BasicEmail: React.FC<Readonly<BasicEmailProps>> = ({
               </Column>
             </Row>
           </Section>
-          <Section className="mt-8 bg-[#fffaed]">
+          <Section className="mt-8 bg-[#fffaed]" align="left">
             <Row>
               <Column className="pt-3 px-7 flex w-full items-center">
                 <Text className="font-medium text-lg">
                   Use Masca Wallet
                   <Img
-                    className="mb-1 ml-0.5 mr-2 w-[16px] h-[15px] inline-flex"
+                    className="ml-0.5 mr-2 w-[16px] h-[15px] inline-flex"
                     width={18}
                     height={16}
                     src={`${process.env.NEXT_PUBLIC_APP_URL}/images/masca_black.png`}
@@ -74,67 +66,96 @@ export const BasicEmail: React.FC<Readonly<BasicEmailProps>> = ({
                 </Text>
               </Column>
             </Row>
-            <Row>
-              <Column className="px-7 flex items-center space-x-1">
-                <WalletIcon className="p-1 mr-2 w-7 h-7 inline-flex text-[#F4D280] bg-[#FCF1D5] border-1 border-[#F4D280] rounded-full" />
-
-                <Text className="font-normal text-base">Download</Text>
-                <Link
-                  className="font-normal text-base underline text-inherit"
-                  href="https://masca.io/"
-                >
-                  Masca Wallet
-                </Link>
+            <Row align="left">
+              <Column className="pl-7 w-10" align="left">
+                <Img
+                  width={28}
+                  height={28}
+                  src={`${process.env.NEXT_PUBLIC_APP_URL}/images/wallet.png`}
+                />
+              </Column>
+              <Column align="left">
+                <Text className="font-normal text-base pl-2">
+                  Download{' '}
+                  <Link
+                    className="font-normal text-base underline text-inherit inline-flex"
+                    href="https://masca.io/"
+                  >
+                    Masca Wallet
+                  </Link>
+                </Text>
               </Column>
             </Row>
-            <Row>
-              <Column className="px-7 flex items-center space-x-1">
-                <DevicePhoneMobileIcon className="p-1 mr-2 w-7 h-7 inline-flex text-[#F4D280] bg-[#FCF1D5] border-1 border-[#F4D280] rounded-full" />
-                <Text className="font-normal text-base">
+            <Row align="left">
+              <Column className="pl-7 w-10" align="left">
+                <Img
+                  width={28}
+                  height={28}
+                  src={`${process.env.NEXT_PUBLIC_APP_URL}/images/phone.png`}
+                />
+              </Column>
+              <Column align="left">
+                <Text className="font-normal text-base pl-2">
                   Connect it to your phone
                 </Text>
               </Column>
             </Row>
-
-            <Row>
-              <Column className="px-7 flex items-center space-x-1">
-                <QrCodeIcon className="p-1 mr-2 w-7 h-7 inline-flex text-[#F4D280] bg-[#FCF1D5] border-1 border-[#F4D280] rounded-full" />
-                <Text className="font-normal text-base">Scan the QR code</Text>
+            <Row align="left">
+              <Column className="pl-7 w-10" align="left">
+                <Img
+                  width={28}
+                  height={28}
+                  src={`${process.env.NEXT_PUBLIC_APP_URL}/images/qr_code.png`}
+                />
+              </Column>
+              <Column align="left">
+                <Text className="font-normal text-base pl-2">
+                  Scan the QR code
+                </Text>
               </Column>
             </Row>
-            <Row>
-              <Column className="pb-3 px-7 flex items-center space-x-1">
-                <KeyIcon className="p-1 mr-2 w-7 h-7 inline-flex text-[#F4D280] bg-[#FCF1D5] border-1 border-[#F4D280] rounded-full" />
-                <Text className="font-normal text-base">
+            <Row align="left">
+              <Column className="pl-7 w-10" align="left">
+                <Img
+                  width={28}
+                  height={28}
+                  src={`${process.env.NEXT_PUBLIC_APP_URL}/images/lock_unlocked.png`}
+                />
+              </Column>
+              <Column align="left">
+                <Text className="font-normal text-base pl-2">
                   Enter PIN from te second email
                 </Text>
               </Column>
             </Row>
           </Section>
-          <Section className="my-6">
+          <Section className="my-6" align="left">
             <Row>
               <Column className="px-7">
                 <Text className="text-lg font-semibold">Why in this form?</Text>
               </Column>
             </Row>
-            <Row>
-              <Column className="pl-7 pr-0.5 w-[149px]">
-                <Row>
-                  <Column className="bg-[#EDF9F3] rounded-md p-2">
-                    <Row>
-                      <Column>
-                        <GlobeAltIcon className="w-5 h-5 inline-flex text-[#2AAA6F]" />
+            <Row align="left">
+              <Column className="pl-7 pr-0.5 max-w-[149px]" align="left">
+                <Row align="left">
+                  <Column className="bg-[#EDF9F3] rounded-md p-2" align="left">
+                    <Row align="left" className="w-full">
+                      <Column align="left">
+                        <Img
+                          className="max-w-[18px] mr-1"
+                          src={`${process.env.NEXT_PUBLIC_APP_URL}/images/globe.png`}
+                        />
                       </Column>
                     </Row>
-                    <Row>
-                      <Column>
-                        <Text className="m-0 my-1 font-medium text-xs">
+                    <Row align="left">
+                      <Column align="left">
+                        <Text className="m-0 my-1 font-semibold text-xs text-left">
                           Standardization
                         </Text>
                       </Column>
                     </Row>
-                    <Row>
-                      <Column>
+                    <Row align="left">
+                      <Column align="left">
                         <Text
                           style={{ fontSize: '10px', lineHeight: '12px' }}
                           className="m-0"
@@ -147,23 +168,26 @@ export const BasicEmail: React.FC<Readonly<BasicEmailProps>> = ({
                   </Column>
                 </Row>
               </Column>
-              <Column className="pr-7 pl-0.5 w-[149px]">
-                <Row>
-                  <Column className="bg-[#EDF9F3] rounded-md p-2">
-                    <Row>
-                      <Column>
-                        <LockClosedIcon className="w-5 h-5 inline-flex text-[#2AAA6F]" />
+              <Column className="pr-7 pl-0.5 max-w-[149px]" align="left">
+                <Row align="left">
+                  <Column className="bg-[#EDF9F3] rounded-md p-2" align="left">
+                    <Row align="left" className="w-full">
+                      <Column align="left">
+                        <Img
+                          className="max-h-[18px] mr-1"
+                          src={`${process.env.NEXT_PUBLIC_APP_URL}/images/lock_locked.png`}
+                        />
                       </Column>
                     </Row>
-                    <Row>
-                      <Column>
-                        <Text className="m-0 my-1 font-medium text-xs">
+                    <Row align="left">
+                      <Column align="left">
+                        <Text className="m-0 my-1 font-semibold text-xs text-left">
                           Security
                         </Text>
                       </Column>
                     </Row>
-                    <Row>
-                      <Column>
+                    <Row align="left">
+                      <Column align="left">
                         <Text
                           style={{ fontSize: '10px', lineHeight: '12px' }}
                           className="m-0"
@@ -177,24 +201,27 @@ export const BasicEmail: React.FC<Readonly<BasicEmailProps>> = ({
                 </Row>
               </Column>
             </Row>
-            <Row className="mt-1.5">
-              <Column className="pl-7 pr-0.5 w-[149px]">
-                <Row>
-                  <Column className="bg-[#EDF9F3] rounded-md p-2 ">
-                    <Row>
-                      <Column>
-                        <ForwardIcon className="w-5 h-5 inline-flex text-[#2AAA6F]" />
+            <Row className="mt-1.5" align="left">
+              <Column className="pl-7 pr-0.5 max-w-[149px]" align="left">
+                <Row align="left">
+                  <Column className="bg-[#EDF9F3] rounded-md p-2" align="left">
+                    <Row align="left" className="w-full">
+                      <Column align="left">
+                        <Img
+                          className="max-w-[20px] mr-1"
+                          src={`${process.env.NEXT_PUBLIC_APP_URL}/images/forward.png`}
+                        />
                       </Column>
                     </Row>
-                    <Row>
-                      <Column>
-                        <Text className="m-0 my-1 font-medium text-xs">
+                    <Row align="left">
+                      <Column align="left">
+                        <Text className="m-0 my-1 font-semibold text-xs text-left">
                           Building the future
                         </Text>
                       </Column>
                     </Row>
-                    <Row>
-                      <Column>
+                    <Row align="left">
+                      <Column align="left">
                         <Text
                           style={{ fontSize: '10px', lineHeight: '12px' }}
                           className="m-0"
@@ -207,23 +234,26 @@ export const BasicEmail: React.FC<Readonly<BasicEmailProps>> = ({
                   </Column>
                 </Row>
               </Column>
-              <Column className="pr-7 pl-0.5 w-[149px]">
-                <Row>
-                  <Column className="bg-[#EDF9F3] rounded-md p-2">
-                    <Row>
-                      <Column>
-                        <AcademicCapIcon className="w-5 h-5 inline-flex text-[#2AAA6F]" />
+              <Column className="pr-7 pl-0.5 max-w-[149px]" align="left">
+                <Row align="left">
+                  <Column className="bg-[#EDF9F3] rounded-md p-2" align="left">
+                    <Row align="left" className="w-full">
+                      <Column align="left">
+                        <Img
+                          className="max-h-[18px] mr-1"
+                          src={`${process.env.NEXT_PUBLIC_APP_URL}/images/academic_hat.png`}
+                        />
                       </Column>
                     </Row>
-                    <Row>
-                      <Column>
-                        <Text className="m-0 my-1 font-medium text-xs">
+                    <Row align="left">
+                      <Column align="left">
+                        <Text className="m-0 my-1 font-semibold text-xs text-left">
                           Microcredentials
                         </Text>
                       </Column>
                     </Row>
-                    <Row>
-                      <Column>
+                    <Row align="left">
+                      <Column align="left">
                         <Text
                           style={{ fontSize: '10px', lineHeight: '12px' }}
                           className="m-0"
@@ -241,30 +271,27 @@ export const BasicEmail: React.FC<Readonly<BasicEmailProps>> = ({
           <Section className="my-8 mb-12">
             <Row>
               <Column>
-                <Text className="text-center text-sm font-medium">
+                <Text className="text-center text-sm font-semibold">
                   Brought to you by
                 </Text>
               </Column>
             </Row>
             <Row>
-              <Column className="pl-7">
+              <Column className="pl-7 pr-4 max-w-16 sm:max-w-24">
                 <Img
-                  height={28}
-                  width={77}
+                  height={24}
                   src={`${process.env.NEXT_PUBLIC_APP_URL}/images/bclab.png`}
                 />
               </Column>
-              <Column>
+              <Column className="max-w-16 px-4 sm:max-w-24">
                 <Img
-                  height={28}
-                  width={100}
+                  height={24}
                   src={`${process.env.NEXT_PUBLIC_APP_URL}/images/eductx.png`}
                 />
               </Column>
-              <Column className="pr-7">
+              <Column className="pr-7 pl-4 max-w-16 sm:max-w-24">
                 <Img
-                  height={28}
-                  width={77}
+                  height={24}
                   src={`${process.env.NEXT_PUBLIC_APP_URL}/images/masca_black_with_text.png`}
                 />
               </Column>
@@ -275,29 +302,31 @@ export const BasicEmail: React.FC<Readonly<BasicEmailProps>> = ({
               <Hr className="border-t-1.5 rounded-md" />
             </Column>
           </Row>
-          <Section className="mt-4 mb-10">
+          <Section className="mt-4 mb-10" align="left">
             <Row>
               <Column className="px-7">
                 <Row className="mt-4">
                   <Column className="px-3">
                     <Img
-                      width={72}
-                      height={72}
+                      width={36}
+                      height={36}
                       src={`${process.env.NEXT_PUBLIC_APP_URL}/images/question_mark.png`}
                     />
                   </Column>
                   <Column className="pr-3">
-                    <Row className="font-medium text-xs">Need some help?</Row>
+                    <Row className="font-semibold text-xs mb-1 text-left">
+                      Need some help?
+                    </Row>
                     <Row>
                       <Text
                         className="m-0"
                         style={{ fontSize: '10px', lineHeight: '12px' }}
                       >
                         If you need any help whatsoever or just want to find out
-                        more about the EduCTX ecosystems, email us anytime at
+                        more about the EduCTX ecosystems, email us anytime at{' '}
                         <Link
                           href="mailto:blockchain-lab@um.si?subject=EduCTX"
-                          className="ml-0.5 inline-flex text-[#2AAA6F]"
+                          className="inline-flex text-[#2AAA6F]"
                           style={{ fontSize: '10px', lineHeight: '12px' }}
                         >
                           blockchain-lab@um.si
@@ -309,7 +338,7 @@ export const BasicEmail: React.FC<Readonly<BasicEmailProps>> = ({
               </Column>
             </Row>
           </Section>
-          <Hr className="h-1 bg-[#2AAA6F] border-[#2AAA6F]" />
+          <Hr className="m-0 h-1 bg-[#2AAA6F] border-[#2AAA6F]" />
           <Section className="bg-[#156166]">
             <Text
               className="px-7 text-white"
