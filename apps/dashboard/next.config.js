b5b8565ba2900361d75d73fd149180d8caffd7cb
Urban Vidovič <urbanfoundit@gmail.com>
2024-08-13 15:55:19 +0200

    chore: add header cors policies
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -2,6 +2,27 @@
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   output: 'standalone',
+  headers: () => {
+    return [
+      {
+        source: '/api/issue-oidc',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization, x-api-key, schemaType',
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.externals.push('bufferutil');
     config.externals.push('utf-8-validate');
