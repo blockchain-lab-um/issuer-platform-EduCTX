ae8ca903f02b40be864c43408d6cf3c10ebffa01
martines3000 <domajnko.martin@gmail.com>
2025-02-12 09:49:23 +0100

    chore: improve text

--- apps/testing-frontend/src/components/TestingView/index.tsx
+++ apps/testing-frontend/src/components/TestingView/index.tsx
@@ -214,11 +214,32 @@ export const TestingView = () => {
         {verificationRequest && disabled && (
           <Button onClick={() => setIsDisabled(false)}>Check status</Button>
         )}
-        {!disabled && (
+        {!false && (
           <div className="flex flex-col justify-center items-center">
             {authRequestStatus?.status === 'Pending' && <div>Loading...</div>}
-            {authRequestStatus?.status === 'Success' && <div>Success</div>}
-            {authRequestStatus?.status === 'Failed' && <div>Failed</div>}
+            {authRequestStatus?.status === 'Success' && (
+              <div className="flex flex-col justify-center items-center text-green-600">
+                <div>Successfully presented credentials.</div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <title>Checkmark</title>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+                  />
+                </svg>
+              </div>
+            )}
+            {authRequestStatus?.status === 'Failed' && (
+              <div className="text-red-600">Verification failed. Error: </div>
+            )}
             {authRequestStatus?.status === 'Failed' &&
               authRequestStatus?.error && (
                 <div className="max-w-xs max-h-32 overflow-y-auto">
