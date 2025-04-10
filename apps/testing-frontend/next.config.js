773aff3fbc0cad18141217ca5252fe9e069cf8dd
martines3000 <domajnko.martin@gmail.com>
2025-04-10 16:14:26 +0200

    chore: remove basePath from interop testing frontend

--- apps/testing-frontend/next.config.js
+++ apps/testing-frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
       process.env.NEXT_PUBLIC_BACKEND_API_URL ||
       'NEXT_PUBLIC_BACKEND_API_URL_PLACEHOLDER',
   },
-  basePath: '/interop-testing-frontend',
+  basePath: '',
   output: 'standalone',
   webpack: (config) => {
     config.externals.push('bufferutil');
