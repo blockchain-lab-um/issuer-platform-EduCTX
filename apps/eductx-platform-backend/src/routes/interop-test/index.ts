532abf9de0a83bff7ea9e5992753816c2b0de0a4
martines3000 <domajnko.martin@gmail.com>
2025-05-19 09:12:43 +0200

    chore: update interop demo logs

--- apps/eductx-platform-backend/src/routes/interop-test/index.ts
+++ apps/eductx-platform-backend/src/routes/interop-test/index.ts
@@ -107,7 +107,7 @@ const route: FastifyPluginAsyncJsonSchemaToTs = async (
       }
 
       const data = await response.json();
-
+      console.log(data);
       if (data.status === 'Success') {
         return reply.code(200).send({
           status: 'Success',
